4a5485291965a3e884be06c50c587103406fe2fd
Ernest Roszak <ernest.roszak89@gmail.com>
2020-07-08 15:53:21 +0200

    js added to editor

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,34 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const entry = localStorage.getItem('entry');
+let result = '';
+
+if (entry){
+result = entry; 
+}
+
+
+
+const content = document.querySelector('.entry--js');
+content.value = result;
+
+const save = document.querySelector('.button__save--js')
+
+save.addEventListener('click', () => {
+    localStorage.setItem('entry', content.value);
+    content.value = '';
+    console.log(entry);
+    
+})
+
+const load = document.querySelector('.button__load--js')
+
+load.addEventListener('click', () => {
+
+    
+    console.log(entry);
+    
+    content.value = localStorage.getItem('entry');;
+})
