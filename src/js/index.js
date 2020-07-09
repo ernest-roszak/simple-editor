f59c7c36e7001eed9e418aa169eb36025802a463
Ernest Roszak <ernest.roszak89@gmail.com>
2020-07-09 15:20:18 +0200

    button clear added

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,35 +6,58 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const content = document.querySelector(".entry--js");
 
-const entry = localStorage.getItem('entry');
-let result = '';
-
-if (entry){
-result = entry; 
+const myValue = localStorage.getItem('entry');
+if (myValue) {
+    document.querySelector('.info--js').innerHTML = 'ℹ️';
 }
 
+const save = document.querySelector(".save--js");
 
+save.addEventListener("click", (e) => {
+    e.preventDefault();
+  localStorage.setItem("entry", content.value);
+  
+  if (content.value) {
+    document.querySelector('.info--js').innerHTML = "ℹ️";
+  } else {
+    document.querySelector('.info--js').innerHTML = "";
+  }
+  content.value = "";
+  document.querySelector('.warning--js').innerHTML = "";
+});
 
-const content = document.querySelector('.entry--js');
-content.value = result;
+const load = document.querySelector(".load--js");
 
-const save = document.querySelector('.button__save--js')
+load.addEventListener("click", (e) => {
+    e.preventDefault();
+  content.value = localStorage.getItem('entry');
+});
 
-save.addEventListener('click', () => {
-    localStorage.setItem('entry', content.value);
-    content.value = '';
-    console.log(entry);
+const clear = document.querySelector(".clear--js");
+
+clear.addEventListener("click", (e) => {
+    e.preventDefault();
+  content.value = localStorage.removeItem('entry');
+  content.value = "";
+  document.querySelector('.info--js').innerHTML = "";
+  document.querySelector('.warning--js').innerHTML = "";
+});
+
+console.log(myValue);
+const warning = document.querySelector('.warning--js')
+content.addEventListener("input", (e) => {
+    e.preventDefault();
+    console.log(content.value);
+if (content.value) {
     
-})
+    document.querySelector('.warning--js').innerHTML = "Masz nie zapisane zmiany";
+  } else {
+    document.querySelector('.warning--js').innerHTML = "";
+  }
+});
 
-const load = document.querySelector('.button__load--js')
 
-load.addEventListener('click', () => {
 
-    
-    console.log(entry);
-    
-    content.value = localStorage.getItem('entry');;
-})
+
